6cb0b59ac810f5826fe304f87fb6713e88e48b9a
SKD3 <132363438+Sucean@users.noreply.github.com>
2026-03-05 11:41:27 +0100

    Add files via upload

--- ui/openfoam_tab.py
+++ ui/openfoam_tab.py
@@ -1,43 +1,88 @@
 import gradio as gr
+import os
+from pathlib import Path
 from ui.components import (
     create_blockmesh_component,
     create_control_dict_component,
     create_fv_schemes_component,
     create_fv_solution_component,
     create_decompose_component,
+    create_surface_component,
     create_snappy_component,
-    create_velocity_component
+    create_velocity_component,
+    create_pressure_component,
+    create_transport_component,
+    create_turbulence_component
 )
+
 
 def create_openfoam_tab():
     """OpenFOAM system files tab"""
-    print(type(gr))
+    
     gr.Markdown("### Generate OpenFOAM System Control Files")
+
+    
+    with gr.Row():
+        working_dir = gr.Textbox(label="Working Directory", value=os.getcwd()) 
+        #update_btn = gr.Button("Update")
+    
+    # Directory browser
+        current_dir = gr.Dropdown(label="Project Directories", info="Available directories in working path", choices=[])
+
+    def update_directories(path):
+        """Update directory list based on working directory"""
+        try:
+            path_obj = Path(path)
+            if path_obj.exists() and path_obj.is_dir():
+                dirs = [d for d in os.listdir(path_obj) if (path_obj / d).is_dir()]
+                return gr.Dropdown(choices=dirs, value=dirs[0] if dirs else None, interactive=True)
+            return gr.Dropdown(choices=[], interactive=True)
+        except Exception:
+            return gr.Dropdown(choices=[], interactive=True)
+    
+    #update_btn.click(fn=update_directories, inputs=[working_dir], outputs=[current_dir])
+    working_dir.change(fn=update_directories, inputs=[working_dir], outputs=[current_dir])
+    
     gr.Markdown("## system/")
     
     stl_file = gr.File(label="Upload STL File", file_types=[".stl"], elem_id="stl_file")
 
     # Load Components
     with gr.Accordion("1. blockMeshDict", open=False):
-        create_blockmesh_component(stl_file)
+        create_blockmesh_component(stl_file, working_dir, current_dir)
 
     with gr.Accordion("2. controlDict", open=False):
-        create_control_dict_component()
+        create_control_dict_component(working_dir, current_dir)
 
     with gr.Accordion("3. fvSchemes", open=False):
-        create_fv_schemes_component()
+        create_fv_schemes_component(working_dir, current_dir)
 
     with gr.Accordion("4. fvSolution", open=False):
-        create_fv_solution_component()
+        create_fv_solution_component(working_dir, current_dir)
 
     with gr.Accordion("5. decomposeParDict", open=False):
-        create_decompose_component()
+        create_decompose_component(working_dir, current_dir)
 
-    with gr.Accordion("6. snappyHexMeshDict", open=False):
-        create_snappy_component()
+    with gr.Accordion("6. surfaceFeatureExtractDict", open=False):
+        create_surface_component(working_dir, current_dir)
 
+    with gr.Accordion("7. snappyHexMeshDict", open=False):
+        create_snappy_component(working_dir, current_dir)
+
+
+    
     gr.Markdown("## 0/")
 
     with gr.Accordion("Velocity U", open=False):
-        create_velocity_component()
-        
+        create_velocity_component(working_dir, current_dir)
+
+    with gr.Accordion("Pressure p", open=False):
+        create_pressure_component(working_dir, current_dir)
+
+    gr.Markdown("## constant/")
+
+    with gr.Accordion("Velocity U", open=False):
+        create_transport_component(working_dir, current_dir)
+
+    with gr.Accordion("Pressure p", open=False):
+        create_turbulence_component(working_dir, current_dir)
--- ui/tiff_tab.py
+++ ui/tiff_tab.py
@@ -10,9 +10,11 @@ import numpy as np
 
 from src.mesh_generator import MeshGenerator
 from src.preprocessor import Preprocessor, PreprocessingConfig
+from src.walking_cubes import WalkingCubes
 
 def browse_directory():
     # This is a hack - opens file browser but user must manually type path
+    # TODO: This does not work
     current_dir = os.getcwd()
     return gr.Textbox(value=current_dir)
 
@@ -23,14 +25,16 @@ def create_tiff_tab():
     
     with gr.Row():
         with gr.Column():
-            working_dir = gr.Textbox(label="Working Directory", value=os.getcwd())           
+            working_dir = gr.Textbox(label="Working Directory", value=os.getcwd()) 
             tiff_file = gr.UploadButton(label="Upload 3D TIFF Stack", file_types=[".tif", ".tiff"], file_count="single")
             block_size = gr.Slider(64, 512, value=256, label="Block Size")
             cap_size = gr.Slider(1, 50, value=10, label="Cap Size")
             cap_axis = gr.Dropdown(choices=['x', 'y', 'z'], value = 'z', label = "Cap Axis")
-            padding_size = gr.Slider(1, 20, value=8, label="Padding Size")
+            padding_size = gr.Slider(0, 20, value=8, label="Padding Size")
             threshold = gr.Slider(0, 255, value=127, label="Marching Cubes Threshold")
+            tolerance = gr.Number(value=0.001, label="Snapping Distance")
             use_parallel = gr.Checkbox(label='Multiprocessing', value = True)
+            use_legacy = gr.Checkbox(label='Use VTK/Legacy (gives faulty STLs)', value = False)
             tiff_btn = gr.Button("Generate Meshes")
         
         with gr.Column():
@@ -39,18 +43,26 @@ def create_tiff_tab():
     
     tiff_btn.click(
         fn=process_tiff_file,
-        inputs=[tiff_file, block_size, cap_size, padding_size, threshold, cap_axis, working_dir, use_parallel],
+        inputs=[tiff_file, block_size, cap_size, padding_size, threshold, cap_axis, working_dir, tolerance, use_parallel, use_legacy],
         outputs=[tiff_output]
     )
 
 
 def mesh_worker(args):
-    threshold, block, block_id, padding_size, working_dir = args
+    threshold, block, block_id, padding_size, working_dir, tolerance, use_legacy = args
 
-    block = np.pad(block, ((padding_size, padding_size), (padding_size, padding_size), (padding_size, padding_size)), mode='constant', constant_values=0)
+    if padding_size > 1:
+        block = np.pad(block, ((padding_size, padding_size), (padding_size, padding_size), (padding_size, padding_size)), mode='constant', constant_values=0)
+
     
-    mesh_generator = MeshGenerator(threshold)
-    mesh = mesh_generator.generate_mesh_from_array(block)
+    mesh_generator = MeshGenerator(threshold, tolerance)
+    
+    if use_legacy:
+        mesh = mesh_generator.generate_mesh_from_array(block)
+    else:
+        walkingcubes = WalkingCubes()
+        walkingcubes.generate_mesh(block)
+        mesh = walkingcubes.mesh
 
     save_path = working_dir / f'block{block_id:03}' / 'constant' / 'trimesh' / f'block{block_id:03}.stl'
     mesh_generator.write_mesh(mesh, save_path)
@@ -59,7 +71,7 @@ def mesh_worker(args):
 
     return block_id, save_path, boundary_edges
 
-def process_tiff_file(tiff_file, block_size, cap_size, padding_size, threshold, cap_axis, working_dir, use_parallel):
+def process_tiff_file(tiff_file, block_size, cap_size, padding_size, threshold, cap_axis, working_dir, tolerance, use_parallel, use_legacy):
 
     if tiff_file is None:
         return "❌ Please upload a TIFF file"
@@ -97,7 +109,7 @@ def process_tiff_file(tiff_file, block_size, cap_size, padding_size, threshold, 
         system_path.mkdir(parents=True, exist_ok=True)
         zero_path.mkdir(parents=True, exist_ok=True)
 
-    args_list = [(threshold, block, i, padding_size, working_dir) for i, block in enumerate(blocks)]
+    args_list = [(threshold, block, i, padding_size, working_dir, tolerance, use_legacy) for i, block in enumerate(blocks)]
     
     if use_parallel:
         with mp.Pool(processes=mp.cpu_count()) as pool:
